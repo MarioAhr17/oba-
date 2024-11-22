8c2dc0fcc89bfcc0c497d304dc39171e1f34e4f8
MarioAhr17 <mariojesushernandezcastellano@gmail.com>
2024-11-22 07:02:03 -0500

    pagina actividades y contenido
    holi

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,12 +6,14 @@
       <v-list density="compact" nav>
         <v-list-item prepend-icon="mdi-view-dashboard" title="Home" to="/"></v-list-item>
         <v-list-item prepend-icon="mdi-forum" title="Objetivos" to="/objetivos"></v-list-item>
-        <v-list-item prepend-icon="mdi-forum" title="Contenidos" to="/contenidos.vue"></v-list-item>
+        <v-list-item prepend-icon="mdi-forum" title="Contenidos" to="/contenidos"></v-list-item>
         <v-list-item prepend-icon="mdi-forum" title="Actividades" to="/actividades"></v-list-item>
       </v-list>
     </v-navigation-drawer>
 
+    
     <v-main class="d-flex align-center justify-center" style="min-height: 10px;">
+      <nuxt />
     </v-main>
   </v-layout>
 </template>
